17d609599de5754b6f2f0270e2e2aa028bca1273
johannes prueller <johannes.prueller@viesure.io>
2020-07-20 12:45:02 +0200

    Initial commit

---
+++ nodejs/README.md
@@ -0,0 +1,7 @@
+# Hello World Node.js
+
+## Requirements
+1. install node from there: [LINK](https://nodejs.org/en/)
+2. navigte into the nodejs folder and execute *npm init* 
+3. install express *npm install express --save*
+4. start with *npm app.js*
---
+++ nodejs/app.js
@@ -0,0 +1,16 @@
+//Require module
+
+const express = require('express')
+
+//Express Initialize
+
+const app = express()
+const port = 8000
+app.listen(port,() => {
+	console.log('listen port 8000')
+})
+
+//create api
+app.get('/hello_world', (req,res)=>{
+	res.json({message: "Hello World!"})
+})
